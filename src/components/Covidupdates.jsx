b7f129aa7834f3b8d6f9bfd9579ba57f8c684ecf
soumyadeepsinha <s.sinha2701@gmail.com>
2021-10-03 11:27:17 +0530

    utilized Material UI Grid system

--- src/components/Covidupdates.jsx
+++ src/components/Covidupdates.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Grid } from '@material-ui/core';
 import './covid.css';
 
 const Covidupdates = () => {
@@ -8,7 +9,6 @@ const Covidupdates = () => {
   useEffect(() => {
     getcovidupdates();
   }, []);
-
 
   const getcovidupdates = async () => {
     const url = `https://api.covid19api.com/summary`
@@ -21,63 +21,67 @@ const Covidupdates = () => {
       console.error(error);
     }
   };
-
-
   return (
     <>
       <main>
         <h1>LIVE COVID UPDATES</h1>
         <h2>COVID-19 INDIA TRACKER</h2>
-        <ul>
-          <li className="card">
+        <Grid container rowSpacing={1} lg={12}>
+          <Grid item lg={4} sm={6} xs={12}><p style={{ color: 'white' }} className="card">
             <div className="card_upper">
               <div className="card_inner">
                 <p className="card_section"> <span>OUR</span> COUNTRY </p>
                 <p className="card_total card_small">{data.Country}</p>
               </div>
             </div>
-          </li>
-          <li className="card">
+          </p>
+          </Grid>
+          <Grid item lg={4} sm={6} xs={12}><p style={{ color: 'white' }} className="card">
             <div className="card_upper_recovered">
               <div className="card_inner">
                 <p className="card_section"> <span>TOTAL</span> CONFIRMED </p>
                 <p className="card_total card_small">{data.TotalConfirmed}</p>
               </div>
             </div>
-          </li>
-          <li className="card">
+          </p>
+          </Grid>
+          <Grid item lg={4} sm={6} xs={12}><p style={{ color: 'white' }} className="card">
             <div className="card_upper_confirmed">
               <div className="card_inner">
                 <p className="card_section"> <span>NEW</span> CONFIRMED </p>
                 <p className="card_total card_small">{data.NewConfirmed}</p>
               </div>
             </div>
-          </li>
-          <li className="card">
+          </p>
+          </Grid>
+          <Grid item lg={4} sm={6} xs={12}><p style={{ color: 'white' }} className="card">
             <div className="card_upper_death">
               <div className="card_inner">
                 <p className="card_section"> <span>TOTAL</span> DEATHS </p>
                 <p className="card_total card_small">{data.TotalDeaths}</p>
               </div>
             </div>
-          </li>
-          <li className="card">
-            <div className="card_upper_active">
+          </p>
+          </Grid>
+          <Grid item lg={4} sm={6} xs={12}><p style={{ color: 'white' }} className="card">
+            <div className="card_upper_death">
               <div className="card_inner">
-                <p className="card_section"> <span>NEW</span> DEATHS </p>
-                <p className="card_total card_small">{data.NewDeaths}</p>
+                <p className="card_section"> <span>TOTAL</span> DEATHS </p>
+                <p className="card_total card_small">{data.TotalDeaths}</p>
               </div>
             </div>
-          </li>
-          <li className="card">
+          </p>
+          </Grid>
+          <Grid item lg={4} sm={6} xs={12}><p style={{ color: 'white' }} className="card">
             <div className="card_upper_update">
               <div className="card_inner">
                 <p className="card_section"> <span>LAST</span> UPDATE AT </p>
                 <p className="card_total card_small">{data.Date}</p>
               </div>
             </div>
-          </li>
-        </ul>
+          </p>
+          </Grid>
+        </Grid>
       </main>
     </>
   );
